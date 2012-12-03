46687cc62784ca7712862b93851f9ca4e18c353b
Graeme Coupar <grambo@grambo.me.uk>
2012-12-03 23:15:16 +0000

    Added enlive & markdown-clj dependencies

--- project.clj
+++ project.clj
@@ -3,5 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [
+                 [org.clojure/clojure "1.4.0"]
+                 [markdown-clj "0.9.11"]
+                 [enlive "1.0.0"]]
   :main nona.core)
