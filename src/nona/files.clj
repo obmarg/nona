714f5e200b83a1512cfe22fdd0feb3fb338fe489
Graeme Coupar <grambo@grambo.me.uk>
2012-12-04 23:43:15 +0000

    dest-path now added to files on loading

--- src/nona/files.clj
+++ src/nona/files.clj
@@ -4,27 +4,57 @@
   )
 
 (defn- split-data
-  "Takes the data from a file map, and splits it into metadata & data"
-  [{:keys [data] :as file-data}]
+  "Takes the data from a page map, and splits it into metadata & data"
+  [{:keys [data] :as page}]
   (if-let [[_ header body] (re-find #"(?ms)---(.*?)---(.*)" data)]
-    (assoc file-data
+    (assoc page
       :metadata (yaml/parse-string header) 
       :data body
       )
-    file-data))
+    page))
 
 (defn- load-source-file
   [file]
   (if-not (.isDirectory file)
-    {:name (.getName file) :path (.getPath file) :data (slurp file)}
+    {:name (.getName file) 
+     :src-path (.getPath file)
+     :data (slurp file)}
     ))
 
-(def load-source-files
-  (comp
-    set
-    (partial map split-data)
-    (partial remove nil?)
-    (partial map load-source-file)
-    file-seq
-    io/file)
-    )
+(defn- relative-path
+  "Calculates a relative path"
+  [base path]
+  (let [base (.toURI (io/as-file base))
+        path (.toURI (io/as-file path))]
+    (.getPath (.relativize base path))
+    ))
+
+(defn- replace-extension
+  "Replaces the extension on a file"
+  [ext file]
+  (let [ext-pos (.lastIndexOf file ".")
+        basename (subs file 0 ext-pos)
+        result (str basename "." ext)]
+    result
+    ))
+
+(defn- add-dest-path
+  [base-path page]
+  (let [src-path (:src-path page)
+        rel-path (relative-path base-path src-path)
+        dest-path (replace-extension "html" rel-path)
+        ]
+  	(assoc page :dest-path dest-path)
+  	))
+
+(defn load-source-files
+  [path]
+  (->> path
+       io/file
+       file-seq
+       (map load-source-file)
+       (remove nil?)
+       (map (partial add-dest-path path))
+       (map split-data)
+       set
+       ))
