d4f0f497cd9395afab6b8f845ad049e2cdb5cec1
Graeme Coupar <grambo@grambo.me.uk>
2012-12-04 22:21:44 +0000

    YAML metadata now loaded from input files

--- project.clj
+++ project.clj
@@ -6,5 +6,6 @@
   :dependencies [
                  [org.clojure/clojure "1.4.0"]
                  [markdown-clj "0.9.11"]
-                 [enlive "1.0.0"]]
+                 [enlive "1.0.0"]
+                 [clj-yaml "0.4.0"]]
   :main nona.core)
--- src/nona/files.clj
+++ src/nona/files.clj
@@ -1,7 +1,17 @@
-
 (ns nona.files
-  (:require [clojure.java.io :as io])
+  (:require [clojure.java.io :as io]
+            [clj-yaml.core :as yaml])
   )
+
+(defn- split-data
+  "Takes the data from a file map, and splits it into metadata & data"
+  [{:keys [data] :as file-data}]
+  (if-let [[_ header body] (re-find #"(?ms)---(.*?)---(.*)" data)]
+    (assoc file-data
+      :metadata (yaml/parse-string header) 
+      :data body
+      )
+    file-data))
 
 (defn- load-source-file
   [file]
@@ -12,6 +22,7 @@
 (def load-source-files
   (comp
     set
+    (partial map split-data)
     (partial remove nil?)
     (partial map load-source-file)
     file-seq
